28ec103f6747d5a9e330c8700f21cf2456cfce21
RyJaxxon <ryanjaxxon@gmail.com>
2024-07-18 13:53:48 -0400

    section 3 tests complete

--- src/2-factory-function.js
+++ src/2-factory-function.js
@@ -1,6 +1,21 @@
-const bankAccountFactory = () => {
-
+const bankAccountFactory = (initialBalance) => {
+  let balance = initialBalance === undefined ? 0 : initialBalance;
+  
+  return {
+    checkBalance : function () {
+      return balance;
+    },
+    add : function (amount) {
+      balance += amount;
+      console.log(`$${amount} added.`);
+    },
+    subtract : function (amount) {
+      balance -= amount;
+      console.log(`$${amount} subtracted.`);
+    }
+  };
 };
+
 
 module.exports = {
   bankAccountFactory,
